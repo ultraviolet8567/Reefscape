3680127db852ef26599a086d5575b0422b38805d
TaoTaoBamBam <srta.taotaobambam@gmail.com>
2025-02-27 16:08:27 -0500

    working swerve, updated deadband

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -159,7 +159,7 @@ public final class Constants {
 		public static final int kDriverControllerPort = 0;
 		public static final int kOperatorControllerPort = 1;
 
-		public static final double kDeadband = 0.1;
+		public static final double kDeadband = 0.01;
 	}
 
 	public static enum RobotType {
