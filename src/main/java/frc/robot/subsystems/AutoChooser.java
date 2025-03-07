93a17437c41cf482a887e010c745e9ae2aefcf12
TaoTaoBamBam <srta.taotaobambam@gmail.com>
2025-03-07 17:31:36 -0500

    Added new autos for starting left

--- src/main/java/frc/robot/subsystems/AutoChooser.java
+++ src/main/java/frc/robot/subsystems/AutoChooser.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -15,8 +16,8 @@ import org.littletonrobotics.junction.Logger;
 
 public class AutoChooser extends VirtualSubsystem {
 	private static final ShuffleboardTab main = Shuffleboard.getTab("Main");
-	private final SendableChooser<String> driveOut;
-	// private final GenericEntry autoName;
+	private final SendableChooser<String> driveOut, coralNumber, startPos, side;
+	private final GenericEntry autoName;
 
 	private final Map<String, PathPlannerAuto> allAutos = new HashMap<String, PathPlannerAuto>();
 
@@ -26,8 +27,42 @@ public class AutoChooser extends VirtualSubsystem {
 		driveOut = new SendableChooser<>();
 		driveOut.setDefaultOption("None", "");
 		driveOut.addOption("move", "move also");
-		main.add("Number of Pieces", driveOut).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
-				.withPosition(3, 1);
+
+		// number of coral
+		coralNumber = new SendableChooser<>();
+		coralNumber.setDefaultOption("0", "do nothing");
+		coralNumber.addOption("1 Coral", "Place 1 ");
+		coralNumber.addOption("2 Coral", "Place 2 ");
+		coralNumber.addOption("3 coral", "Place 3 ");
+		coralNumber.addOption("4 coral", "Place 4 ");
+
+		// pos selection
+		startPos = new SendableChooser<>();
+		startPos.setDefaultOption("None", "");
+		startPos.addOption("Center", "Middle ");
+		startPos.addOption("Left", "Left ");
+		startPos.addOption("Right", "Right ");
+
+		// reef side selection
+		side = new SendableChooser<>();
+		side.setDefaultOption("None", "");
+		side.addOption("Reef Side 0", "Side 0");
+		side.addOption("Reef Side 1", "Side 1");
+		side.addOption("Reef Side 2", "Side 2");
+		side.addOption("Reef Side 3", "Side 3");
+		side.addOption("Reef Side 4", "Side 4");
+		side.addOption("Reef Side 5", "Side 5");
+
+		// add selectors to shuffleboard
+
+		main.add("Drive Out", driveOut).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1).withPosition(3, 1);
+		main.add("Number of Coral", coralNumber).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
+				.withPosition(6, 1);
+		main.add("Start Location", startPos).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1).withPosition(9,
+				1);
+		main.add("Reef Side", side).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1).withPosition(3, 3);
+		autoName = main.add("Auto Name", "").withWidget(BuiltInWidgets.kTextView).withSize(2, 1).withPosition(3, 4)
+				.getEntry();
 
 		/*
 		 * pieceNumber = new SendableChooser<>(); u // noteNumber.addOption("1 Note",
@@ -68,22 +103,28 @@ public class AutoChooser extends VirtualSubsystem {
 	public void periodic() {
 		Logger.recordOutput("Auto/Routine", getAutoCommandName());
 
-		// autoName.setString(
-		// allAutos.containsKey(getAutoCommandName()) ? getAutoCommandName() : "Auto
-		// routine does not exist");
+		autoName.setString(
+				allAutos.containsKey(getAutoCommandName()) ? getAutoCommandName() : "Auto routine does not exist");
 	}
 
 	// Returns name of pre-defined autonomous command based on Shuffleboard input
 	public String getAutoCommandName() {
-		return "Do Nothing";
-		// if (pieceNumber.getSelected().equals("Do Nothing")) {
-		// return "Do Nothing";
-		// } else if (otherStuff.getSelected().equals("Test")) {
-		// return "Test";
-		// } else {
-		// return pieceNumber.getSelected() + " " + otherStuff.getSelected() +
-		// sideOfField.getSelected();
-		// }
+
+		if (coralNumber.getSelected().equals("Do Nothing")) {
+			return "Do Nothing";
+
+		}
+
+		else if (side.getSelected().equals("")) {
+			return " Do Nothing";
+
+		}
+
+		else if (startPos.getSelected().equals("")) {
+			return "Do Nothing";
+		} else {
+			return "Start " + startPos.getSelected() + coralNumber.getSelected() + side.getSelected();
+		}
 	}
 
 	public Pose2d getAutoStartingPose() {
